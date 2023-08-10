d6cd8b0fbf6bc44afefaeb9187cb9e843050e356
Bpalacio <bpalacio.ext@iseazy.com>
2023-08-11 00:42:55 +0200

    Auto stash before merge of "feature/refactor" and "origin/feature/refactor"

--- apps/new-manager/src/store/usePathList.ts
+++ apps/new-manager/src/store/usePathList.ts
@@ -1,11 +1,35 @@
 import { produce } from 'immer';
 import { create } from 'zustand';
-
-//devtools
 import { mountStoreDevtool } from 'simple-zustand-devtools';
-
 import { v4 as uuidv4 } from 'uuid';
 
-import {
-  Element
-} from './interfaces/pathList.interface';
+import { Element } from './interfaces/pathList.interface';
+
+export type State = {
+  elements: Element[];
+
+  addElement: (element: Element) => void;
+};
+
+const initialState: State = {
+  elements: [],
+  addElement: () => {},
+};
+
+const useSectionsStore = create<State>((set) => ({
+  ...initialState,
+
+  addElement: (element) => {
+    set((state) =>
+      produce(state, (draft) => {
+        draft.elements.push(element);
+      })
+    );
+  },
+}));
+
+if (process.env.NODE_ENV === 'development') {
+  mountStoreDevtool('SectionsStore', useSectionsStore);
+}
+
+export default useSectionsStore;
